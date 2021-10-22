ced6b0e38dcbb6b07a65f11381e9aa775655d14f
Mark Brocato <mark.brocato@moovweb.com>
2021-10-22 17:26:24 +0300

    Implement verifyJWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,45 +6,9 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
-    Providers.Email({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-    /*
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
-      scope: "read:user",
-    }),
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    Providers.Twitter({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Providers.Auth0({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      domain: process.env.AUTH0_DOMAIN,
     }),
   ],
   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
@@ -53,7 +17,7 @@ export default NextAuth({
   // Notes:
   // * You must install an appropriate node_module for your database
   // * The Email provider requires a database (OAuth providers do not)
-  database: process.env.DATABASE_URL,
+  // database: process.env.DATABASE_URL,
 
   // The secret should be set to a reasonably long random string.
   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
@@ -98,7 +62,7 @@ export default NextAuth({
   // pages is not specified for that route.
   // https://next-auth.js.org/configuration/pages
   pages: {
-    // signIn: '/auth/signin',  // Displays signin buttons
+    // signIn: "/auth/signin", // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
     // error: '/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // Used for check email page
@@ -124,5 +88,5 @@ export default NextAuth({
   theme: "light",
 
   // Enable debug messages in the console if you are having problems
-  debug: false,
+  debug: true,
 });
--- routes.js
+++ routes.js
@@ -1,10 +1,22 @@
-// This file was automatically added by layer0 deploy.
-// You should commit this file to source control.
-const { Router } = require('@layer0/core/router')
-const { nextRoutes } = require('@layer0/next')
+import { Router } from "@layer0/core";
+import { nextRoutes } from "@layer0/next";
 
 module.exports = new Router()
-  .match('/service-worker.js', ({ serviceWorker }) => {
-    return serviceWorker('.next/static/service-worker.js')
+  .match("/service-worker.js", ({ serviceWorker }) => {
+    return serviceWorker(".next/static/service-worker.js");
   })
-  .use(nextRoutes) // automatically adds routes for all files under /pages
+  .match("/protected/static", async ({ verifyJWT }) => {
+    await verifyJWT({
+      cookie: process.env.JWT_COOKIE || "next-auth.session-token",
+      secret: process.env.SIGNING_KEY,
+      redirectTo: "/api/auth/signin",
+    });
+  })
+  .match("/_next/data/:version/protected/:page*", async ({ verifyJWT }) => {
+    await verifyJWT({
+      cookie: process.env.JWT_COOKIE || "next-auth.session-token",
+      secret: process.env.SIGNING_KEY,
+      redirectTo: "/api/auth/signin",
+    });
+  })
+  .use(nextRoutes); // automatically adds routes for all files under /pages
