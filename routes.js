b3bf135b56761df38deab64507aeefe5939197fc
Tristan Lee <tristan.lee@moovweb.com>
2022-06-22 12:08:51 -0400

    Test with hardcoded router configuration

--- routes.js
+++ routes.js
@@ -7,19 +7,18 @@ export default new Router()
   })
   .match("/protected/static", ({ verifyJwt }) => {
     verifyJwt({
-      algo: "HS512",
-      header: "Authorization",
-      cookie: process.env.JWT_COOKIE || "next-auth.session-token",
-      secret: process.env.JWT_SECRET,
+      algo: "HS256",
+      cookie: "next-auth.session-token",
+      secret: "secret",
       redirectInvalid: "/api/auth/signin",
+      redirectExpiredAbsent: "/api/auth/signin",
     });
   })
   .match("/_next/data/:version/protected/:page*", ({ verifyJwt }) => {
     verifyJwt({
-      algo: "HS512",
-      header: "Authorization",
-      cookie: process.env.JWT_COOKIE || "next-auth.session-token",
-      secret: process.env.JWT_SECRET,
+      algo: "HS256",
+      cookie: "next-auth.session-token",
+      secret: "secret",
       redirectInvalid: "/api/auth/signin",
     });
   })
